6175ac79c98d02314fb14e7750d32ee2c9a3953a
Serg Eroshe <itechmeat@gmail.com>
2022-11-06 19:47:08 +0100

    feat: projects crud #11 (#12)
    Co-authored-by: techmeat <T5Kf8pY9w9yRVLs>

--- src/pages/ProjectsListPage.tsx
+++ src/pages/ProjectsListPage.tsx
@@ -1,30 +1,52 @@
 import { FC } from 'react'
-import { Button } from 'ariakit/button'
 import { NavLink } from 'react-router-dom'
+import { gql, useQuery } from '@apollo/client'
+import { useAuthenticated } from '@nhost/react'
+
+const GET_PROJECTS = gql`
+  query MyProjects {
+    projects {
+      id
+      name
+      description
+    }
+  }
+`
 
 type ProjectsListPageType = {}
 
 export const ProjectsListPage: FC<ProjectsListPageType> = () => {
+  const isAuthenticated = useAuthenticated()
+  const { loading, data, error } = useQuery(GET_PROJECTS)
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (!isAuthenticated) {
+    return <div>You must be authenticated to see this page</div>
+  }
+
+  if (error) {
+    return <div>Error in the query {error.message}</div>
+  }
+
   return (
     <div>
       <h1>Your projects</h1>
 
       <ul>
-        <li>
-          <NavLink to="1">ProjectsItemPage #1</NavLink>
-        </li>
-        <li>
-          <NavLink to="2">ProjectsItemPage #2</NavLink>
-        </li>
-        <li>
-          <NavLink to="3">ProjectsItemPage #3</NavLink>
-        </li>
+        {data?.projects?.map((project: any) => (
+          <li key={project.id}>
+            <NavLink to={project.id}>{project.name}</NavLink>
+          </li>
+        ))}
       </ul>
 
       <p>
-        <Button as="a" href="/projects/create" className="button">
+        <NavLink to="create" className="button">
           Add new project
-        </Button>
+        </NavLink>
       </p>
     </div>
   )
